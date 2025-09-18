2911a79ca177e82b84974de7ab477f47bcfa7e1f
monkeyreel <Radostevk80@yandex.ru>
2025-09-18 19:02:38 +0500

    modified:   parabolic_method.py 	modified:   tangent_method.py

--- parabolic_method.py
+++ parabolic_method.py
@@ -1,22 +1,25 @@
-import math
+import math as m
 
 def f(x):
-    return (x - 4)**2 + math.log(x)
+    return (x-4)**2+m.log(x)
 
 def f_dx(x):
-    return 2*(x - 4) + 1/x
+    return 2*(x-4) + 1/x
 
 def f_ddx(x):
-    return 2 - 1/(x**2)
+    return 2 - 1/x**2
 
-def newton_method(x0, eps=1e-6, max_iter=100):
-    x = x0
-    for i in range(max_iter):
-        x_new = x - f_dx(x)/f_ddx(x)
-        if abs(x_new - x) < eps:
-            return x_new, f(x_new), i+1
+def parabolic_method(x, eps=10**(-6)):
+    count = 0
+    x_new = x - f_dx(x)/f_ddx(x)
+    while abs(x_new-x) > eps:
         x = x_new
-    return x, f(x), max_iter
+        x_new = x - f_dx(x)/f_ddx(x)
+        count += 1
+    return x_new, count
 
-xmin, fmin, steps = newton_method(2.0)
-print(f"Минимум в точке x = {xmin}, f(x) = {fmin}, шагов = {steps}")
+xmin, count = parabolic_method(3)
+
+print()
+print('-------Метод парабол-------')
+print(f'Xmin={xmin:.4f}, f(xmin)={f(xmin):.4f}, count={count}')
--- tangent_method.py
+++ tangent_method.py
@@ -1,33 +1,38 @@
 import math as m
 
-
 def f(x):
     return (x-4)**2+m.log(x)
 
 def f_dx(x):
     return 2*(x-4) + 1/x
 
+def tangent(x, x0):
+    return f(x0)+f_dx(x0)*(x-x0)
 
-def tangent_method(a, b, eps=10**(-12)):
-    count = 0
-    x1 = (f(a)-f(b)-f_dx(a)*a+f_dx(b)*b)/(f_dx(b)-f_dx(a)) # x1 - пересечение касательных в точках a и b (4.00582)
-    x2 = (f(a)-f(x1)-f_dx(a)*a+f_dx(x1)*x1)/(f_dx(x1)-f_dx(a)) # x2 - пересечение касательных в точках a и x1 (3.50501)
-    x3 = (f(x1)-f(b)-f_dx(x1)*a+f_dx(b)*b)/(f_dx(b)-f_dx(x1)) # x3 - пересечение касательных в точках b и x1 (4.50385)
+def cross_line(a, b):
+    return (f(a)-f(b)-f_dx(a)*a+f_dx(b)*b)/(f_dx(b)-f_dx(a)) 
     
-    while abs(b-a) >= eps:
-        if f(x2) < f(x3):
-            a, b = x2, x1
-            x1 = (f(a)-f(b)-f_dx(a)*a+f_dx(b)*b)/(f_dx(b)-f_dx(a))
-            x2 = (f(a)-f(x1)-f_dx(a)*a+f_dx(x1)*x1)/(f_dx(x1)-f_dx(a))  
-            x3 = (f(x1)-f(b)-f_dx(x1)*a+f_dx(b)*b)/(f_dx(b)-f_dx(x1)) 
-        else:
-            a, b = x1, x3
-            x1 = (f(a)-f(b)-f_dx(a)*a+f_dx(b)*b)/(f_dx(b)-f_dx(a))
-            x2 = (f(a)-f(x1)-f_dx(a)*a+f_dx(x1)*x1)/(f_dx(x1)-f_dx(a)) 
-            x3 = (f(x1)-f(b)-f_dx(x1)*a+f_dx(b)*b)/(f_dx(b)-f_dx(x1))
-        count += 1 
 
-    print(x1, f(x1), count)
+def tangent_method(a, b, eps=10**(-6)):
+    count = 0
+    x_m = cross_line(a, b) # 4.0
+    x_l = cross_line(a, x_m) # 3.5
+    x_r = cross_line(x_m, b) # 4.6
+    while abs(b-a) > eps and abs(f(x_m)-tangent(x_m, b)) > eps:
+        if f(x_l) < f(x_r):
+            a, b = x_l, x_m
+            x_m = cross_line(a, b)
+            x_l = cross_line(a, x_m) 
+            x_r = cross_line(x_m, b) 
+        else: 
+            a, b = x_m, x_r
+            x_m = cross_line(a, b) 
+            x_l = cross_line(a, x_m) 
+            x_r = cross_line(x_m, b) 
+        count += 1
+    return x_m, count
 
-
-tangent_method(3, 5)
+xmin, count = tangent_method(3, 5)
+print()
+print('-------Метод касательных-------')
+print(f'Xmin={xmin:.4f}, f(xmin)={f(xmin):.4f}, count={count}')
